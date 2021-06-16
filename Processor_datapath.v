c5079353ff89bdbaeae7ac1eeff6449e341410b3
Sandushan <44228796+Sandushan@users.noreply.github.com>
2021-06-16 14:34:33 +0530

    Change the datapath.v and BUS.v files according to the new control signals

--- Processor_datapath.v
+++ Processor_datapath.v
@@ -4,25 +4,15 @@ input [15:0] dram_in;
 input [15:0] iram_in;
 input[22:0] control;
 output[15:0] bus_out;
-<<<<<<< Updated upstream:Processor_datapath.v
-output wire[15:0] addr_out, ir_out;
-
-wire[15:0] ar,pc,dr,r,ac,alu_out, data_in_alu, alu;
-=======
 output wire[15:0] addr_out, ir_out, pc_addr;
 wire[15:0] ar,pc,dr,r,ac,alu_out, alu;
->>>>>>> Stashed changes:datapath.v
 
 wire[7:0] ir;
 
 
-<<<<<<< Updated upstream:Processor_datapath.v
-BUS BUS(ar,pc,ir_out,dr,r,ac,dram_in,iram_in, control[3:0], bus_out, addr_out);
-=======
 BUS BUS(ar,pc,ir_out,dr,r,ac,dram_in,iram_in, control[7:2], bus_out, addr_out, pc_addr,control[21:20]);
 
 register R(clock,control[14], bus_out, r);
->>>>>>> Stashed changes:datapath.v
 
 register AR(clock,control[15], bus_out, ar);
 
