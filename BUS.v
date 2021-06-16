7427c7eb65889f42d9fe223c4ed998455209b5da
Sandushan <44228796+Sandushan@users.noreply.github.com>
2021-06-16 14:30:56 +0530

    Change the datapath.v and BUS.v files according to the new control signals

--- BUS.v
+++ BUS.v
@@ -1,20 +1,33 @@
+<<<<<<< Updated upstream
 module BUS(ar,pc,ir,dr,r,ac,dram,iram, control, data_out, addr);
+=======
+module BUS(ar,pc,ir,dr,r,ac,dram,iram, control_register, data_out, addr, pc_addr, control_memory);
+>>>>>>> Stashed changes
 
 
 input[15:0] ar,pc,dr,ir, r,ac,dram, iram;
-input[3:0] control;
+input[5:0] control_register;
+input[1:0] control_memory;
 
-
+reg[7:0] control;
 output reg[15:0] data_out;
 
 output reg[15:0] addr;
 
+<<<<<<< Updated upstream
 always @(ar or pc or dr or ir or r or ac or dram or iram or control) //control
 
+=======
+// always @(ar or pc or dr or ir or r or ac or dram or iram or control) //control
+always @(control_register or control_memory)
+>>>>>>> Stashed changes
 begin
 
+control[5:0] = control_register;
+control[7:6] = control_memory;
 case(control)
 
+<<<<<<< Updated upstream
 4'd1: data_out <= ar;
 4'd2: data_out <= ac;
 4'd3: data_out <= pc;
@@ -22,6 +35,16 @@ case(control)
 4'd5: data_out <= r;
 4'd6: data_out <= iram;
 4'd7: data_out <= dram;
+=======
+8'd1: data_out <= r;
+8'd2: data_out <= ar;
+8'd4: data_out <= dr;
+8'd8: data_out <= ac;
+8'd16: data_out <= pc;
+8'd32: data_out <= ir;
+8'd64: data_out <= dram;
+8'd128: data_out <= iram;
+>>>>>>> Stashed changes
 default: data_out <= 16'd0;
 
 endcase
--- Processor_datapath.v
+++ Processor_datapath.v
@@ -2,30 +2,40 @@ module datapath(clock, dram_in, iram_in, control,ir_out, bus_out, addr_out);
 input clock;
 input [15:0] dram_in;
 input [15:0] iram_in;
-input[15:0] control;
+input[22:0] control;
 output[15:0] bus_out;
+<<<<<<< Updated upstream:Processor_datapath.v
 output wire[15:0] addr_out, ir_out;
 
 wire[15:0] ar,pc,dr,r,ac,alu_out, data_in_alu, alu;
+=======
+output wire[15:0] addr_out, ir_out, pc_addr;
+wire[15:0] ar,pc,dr,r,ac,alu_out, alu;
+>>>>>>> Stashed changes:datapath.v
 
 wire[7:0] ir;
 
 
+<<<<<<< Updated upstream:Processor_datapath.v
 BUS BUS(ar,pc,ir_out,dr,r,ac,dram_in,iram_in, control[3:0], bus_out, addr_out);
+=======
+BUS BUS(ar,pc,ir_out,dr,r,ac,dram_in,iram_in, control[7:2], bus_out, addr_out, pc_addr,control[21:20]);
 
-register R(clock,control[3], bus_out, r);
+register R(clock,control[14], bus_out, r);
+>>>>>>> Stashed changes:datapath.v
 
-register AR(clock,control[4], bus_out, ar);
+register AR(clock,control[15], bus_out, ar);
 
-register DR(clock,control[5], bus_out, dr);
+register DR(clock,control[16], bus_out, dr);
 
-AC AC(clock,control[6], bus_out, alu_out, data_in_alu, ac);
+AC AC(clock,control[17], bus_out, control[22], alu_out , ac);
 
-PC PC(clock, control[7], control[8], bus_out, pc);
+PC PC(clock, control[18], control[8], bus_out, pc);
 
-IR IR(clock,control[9], bus_out, ir_out);
+IR IR(clock,control[19], bus_out, ir_out);
 
-ALU ALU(clock, control[11:10], ac, bus_out, alu_out);
+ALU ALU(clock, control[9:8], ac, bus_out, alu_out);
+
 
 endmodule  
 
