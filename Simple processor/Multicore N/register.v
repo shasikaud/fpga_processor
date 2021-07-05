43b1cd1411054d74ccbf104057c836a60311883f
Sandushan <44228796+Sandushan@users.noreply.github.com>
2021-07-05 10:38:21 +0530

    remove unwanted read en

--- Simple processor/Multicore N/register.v
+++ Simple processor/Multicore N/register.v
@@ -1,10 +1,10 @@
-module register(clock, data_out, data_in, read_en, write_en);
+module register(clock, data_out, data_in, write_en);
 
-	input clock,read_en, write_en;
+	input clock, write_en;
 	input [15:0] data_in;
 	output reg[15:0] data_out;
 	
-	reg[15:0] data_store;
+
 	
 
 	 
@@ -15,11 +15,6 @@ module register(clock, data_out, data_in, read_en, write_en);
 	begin
 	data_out <= data_in;
 	end
-	
-//	if(read_en == 1)
-//	begin
-//		data_out <= data_store;
-//	end
 
 	end
 	
